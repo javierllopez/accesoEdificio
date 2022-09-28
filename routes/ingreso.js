f58242e109ca4b496298a945628dca4cb0fc5deb
Javier Lopez <javier_l@yahoo.com>
2022-09-27 22:08:28 -0300

    otro commit

--- routes/ingreso.js
+++ routes/ingreso.js
@@ -2,24 +2,25 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const db = require('../baseDeDatos');
-const {accesoNivel1} = require('../lib/autenticar');
+const {accesoNivel2} = require('../lib/autenticar');
+const ahora = require('../lib/ahora');
 
-router.get('/',accesoNivel1, async (req,res,next)=> {
+router.get('/',accesoNivel2, async (req,res,next)=> {
 
     res.render('ingreso');
 });
-router.post('/',accesoNivel1,async (req,res,next)=> {
+router.post('/',accesoNivel2,async (req,res,next)=> {
     const nuevoAcceso = {
         idEdificio: req.user.edificio,
         idUsuario: req.user.username,
         idPersona: req.body.DNIPersona,
         apellido: req.body.apellidoPersona,
-        nombres: req.body.nombresPersona,
-        fecha: new Date().toLocaleDateString(),
+        nombre: req.body.nombresPersona,
+        fecha: ahora.ahora(),
         hora: new Date().toLocaleTimeString(),
         actividad: 'Ingreso'
     }
-
+    console.log(await ahora);
     const nuevoAccesoEdificio = {
         idEdificio: req.user.edificio,
         idPersona: req.body.DNIPersona,
@@ -28,7 +29,7 @@ router.post('/',accesoNivel1,async (req,res,next)=> {
     }
 
     const regActividad = await db.query('INSERT INTO registroactividad SET ?',[nuevoAcceso]);
-    const regEdificio = await dbquery('INSERT INTO personasenedificio SET ?',[nuevoAccesoEdificio]);
+    const regEdificio = await db.query('INSERT INTO personasenedificio SET ?',[nuevoAccesoEdificio]);
     
     
     req.flash('mensaje','Ingreso registrado exitosamente');   
