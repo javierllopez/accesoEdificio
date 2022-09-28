074e15b2062e26d71c54234546c2f8c5242f965a
Javier Lopez <javier_l@yahoo.com>
2022-09-27 22:39:37 -0300

    otro commit

--- routes/ingreso.js
+++ routes/ingreso.js
@@ -2,14 +2,33 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const db = require('../baseDeDatos');
-const {accesoNivel2} = require('../lib/autenticar');
+const { accesoNivel2 } = require('../lib/autenticar');
 const ahora = require('../lib/ahora');
 
-router.get('/',accesoNivel2, async (req,res,next)=> {
+router.get('/', accesoNivel2, async (req, res, next) => {
 
     res.render('ingreso');
 });
-router.post('/',accesoNivel2,async (req,res,next)=> {
+router.post('/', accesoNivel2, async (req, res, next) => {
+
+    if (req.body.DNIPersona < 1000000) {
+        req.flash('error', 'Debe ingresar un D.N.I. válido');
+        return res.redirect('/ingreso');
+    }
+    if (req.body.apellidoPersona == '') {
+        req.flash('error', 'Debe ingresar el apellido de la persona');
+        return res.redirect('/ingreso');
+    }
+    if (req.body.nombresPersona == '') {
+        req.flash('error', 'Debe ingresar el nombre de la persona');
+        return res.redirect('/ingreso');
+    }
+
+    const estaAdentro = await db.query('SELECT * FROM personasenedificio WHERE idEdificio = ? AND idPersona = ?', [parseInt(req.user.edificio), parseInt(req.body.DNIPersona)]);
+    if (estaAdentro.length > 0) {
+        req.flash('error', 'Esta persona ya ingresó al edificio');
+        return res.redirect('/ingreso');
+    }
     const nuevoAcceso = {
         idEdificio: req.user.edificio,
         idUsuario: req.user.username,
@@ -28,11 +47,11 @@ router.post('/',accesoNivel2,async (req,res,next)=> {
         nombres: req.body.nombresPersona
     }
 
-    const regActividad = await db.query('INSERT INTO registroactividad SET ?',[nuevoAcceso]);
-    const regEdificio = await db.query('INSERT INTO personasenedificio SET ?',[nuevoAccesoEdificio]);
-    
-    
-    req.flash('mensaje','Ingreso registrado exitosamente');   
+    const regActividad = await db.query('INSERT INTO registroactividad SET ?', [nuevoAcceso]);
+    const regEdificio = await db.query('INSERT INTO personasenedificio SET ?', [nuevoAccesoEdificio]);
+
+
+    req.flash('mensaje', 'Ingreso registrado exitosamente');
     res.redirect('/');
 
 });
