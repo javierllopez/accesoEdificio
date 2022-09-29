658c460df85f2d5d551e8643c58632f22631b1be
Javier Lopez <javier_l@yahoo.com>
2022-09-29 00:48:24 -0300

    commit

--- clavesHeroku.js
+++ clavesHeroku.js
@@ -3,6 +3,7 @@ module.exports = {
         host: "migae5o25m2psr4q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         user: "tox0uxj4jzjk70zi",
         password: "vmxxr3bvtdsfx26f",
-        database: "bcucn0985tatdoah"
+        database: "bcucn0985tatdoah",
+        acquireTimeout: 300
     }
 };
