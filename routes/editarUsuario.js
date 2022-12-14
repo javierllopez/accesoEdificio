8d788002b047ee0bb4ac7e1bc461d64c0f90fb3e
Javier Lopez <javier_l@yahoo.com>
2022-12-13 22:06:55 -0300

    modificacion

--- routes/editarUsuario.js
+++ routes/editarUsuario.js
@@ -10,19 +10,21 @@ router.get('/:idUsuario', accesoNivel1, async (req, res) => {
     const idUsuario = req.params;
 
     const resultado = await db.query('SELECT * FROM usuarios WHERE username = ?', [idUsuario.idUsuario]);
-    const edificio = await db.query('SELECT * FROM edificios WHERE administrador = ?',[req.user.id]);
-    const edificioUsuario = await db.query('SELECT * FROM edificiosusuarios WHERE idUsuario = ? AND idAdmin = ?',[idUsuario.idUsuario,req.user.username]);
+    const edificio = await db.query('SELECT * FROM edificios WHERE administrador = ?', [req.user.id]);
+    const edificioUsuario = await db.query('SELECT * FROM edificiosusuarios WHERE idUsuario = ? AND idAdmin = ?', [idUsuario.idUsuario, req.user.username]);
 
-    return res.render('editarUsuario', { usuario: resultado[0], edificio: edificio,edificioUsuario: edificioUsuario[0], helpers: {
-        seleccionado: (esteUsuario, esteEdificio)=> {
-            console.log(esteUsuario, esteEdificio);
-            if (parseInt(esteUsuario) == parseInt(esteEdificio)) return 'selected';
-        },
-        aux: ()=> {
-            return edificioUsuario[0].idEdificio;
+    return res.render('editarUsuario', {
+        usuario: resultado[0], edificio: edificio, edificioUsuario: edificioUsuario[0], helpers: {
+            seleccionado: (esteUsuario, esteEdificio) => {
+                console.log(esteUsuario, esteEdificio);
+                if (parseInt(esteUsuario) == parseInt(esteEdificio)) return 'selected';
+            },
+            aux: () => {
+                return edificioUsuario[0].idEdificio;
+            }
         }
-    }});
-    
+    });
+
 });
 
 router.post('/:idUsuario', accesoNivel1, async (req, res, next) => {
@@ -31,46 +33,48 @@ router.post('/:idUsuario', accesoNivel1, async (req, res, next) => {
 
     if (isNaN(parseInt(req.body.edificioUsuario))) {
         req.flash('error', 'Debe seleccionar un edificio');
-        return res.redirect('/editarUsuario/'+idUsuario.idUsuario);
+        return res.redirect('/editarUsuario/' + idUsuario.idUsuario);
     };
 
     if (req.body.nombreUsuario == '') {
         req.flash('error', 'Debe ingresar el nombre completo del usuario');
-        return res.redirect('/editarUsuario/'+idUsuario.idUsuario);
+        return res.redirect('/editarUsuario/' + idUsuario.idUsuario);
     }
     if (req.body.apellidoUsuario == '') {
         req.flash('error', 'Debe ingresar el apellido del usuario');
-        return res.redirect('/editarUsuario/'+idUsuario.idUsuario);
+        return res.redirect('/editarUsuario/' + idUsuario.idUsuario);
     }
     if (parseInt(req.body.DNIUsuario) < 1000000) {
         req.flash('error', 'Debe ingresar un DNI válido');
-        return res.redirect('/editarUsuario/'+idUsuario.idUsuario);
+        return res.redirect('/editarUsuario/' + idUsuario.idUsuario);
     }
-    const resDni = await db.query('SELECT * FROM usuarios WHERE id = ? AND username != ?', [req.body.DNIUsuario,idUsuario.idUsuario]);
-    if (resDni.length > 0) {
-        req.flash('error', 'El DNI del usuario ya fue cargado');
-        return res.render('/editarUsuario/'+idUsuario.idUsuario);
-    }
+    let usuarioCambiado = false;
+    let usuarioAnterior = '';
+    const resDni = await db.query('SELECT * FROM usuarios WHERE id = ?', [req.body.DNIUsuario]);
     if (req.body.username == '') {
         req.flash('error', 'Debe seleccionar una denominación para el usuario');
-        return res.redirect('/editarUsuario/'+idUsuario.idUsuario);
+        return res.redirect('/editarUsuario/' + idUsuario.idUsuario);
     }
-    const rUsername = await db.query('SELECT * FROM usuarios WHERE username = ? AND id!= ?', [req.body.username,req.body.DNIUsuario]);
+    if (resDni[0].username != req.body.username) {
+        usuarioAnterior = resDni[0].username;
+        usuarioCambiado = true; //Si cambio el nombre de usuario activo la bandera, para cambiar en todas las tablas el nombre de usuario
+    }
+    const rUsername = await db.query('SELECT * FROM usuarios WHERE username = ? AND id!= ?', [req.body.username, req.body.DNIUsuario]);
     if (rUsername.length > 0) {
         req.flash('error', 'El nombre de usuario ya fue ingresado');
-        return res.redirect('/editarUsuario/'+idUsuario.idUsuario);
+        return res.redirect('/editarUsuario/' + idUsuario.idUsuario);
     }
     if (req.body.password.length < 6) {
         req.flash('error', 'La longitud del password no debe ser menor a 6 caracteres');
-        return res.redirect('/editarUsuario/'+idUsuario.idUsuario);
+        return res.redirect('/editarUsuario/' + idUsuario.idUsuario);
     }
     if (req.body.password !== req.body.password2) {
         req.flash('error', 'Las claves ingresadas no coinciden');
-        return res.redirect('/editarUsuario/'+idUsuario.idUsuario);
+        return res.redirect('/editarUsuario/' + idUsuario.idUsuario);
     }
 
     const nuevoUsuario = {
-        id: parseInt(req.body.DNIUsuario),
+        //id: parseInt(req.body.DNIUsuario),
         apellido: req.body.apellidoUsuario,
         nombres: req.body.nombreUsuario,
         username: req.body.username,
@@ -84,9 +88,21 @@ router.post('/:idUsuario', accesoNivel1, async (req, res, next) => {
         idEdificio: parseInt(req.body.edificioUsuario),
     }
 
-    await db.query('UPDATE usuarios SET ? WHERE username = ?', [nuevoUsuario,req.body.username]);
+    await db.query('UPDATE usuarios SET ? WHERE id = ?', [nuevoUsuario, parseInt(req.body.DNIUsuario)]);
 
-    await db.query('UPDATE edificiosusuarios SET ? WHERE idUsuario = ? AND idAdmin = ?', [nuevoEdificioUsuario,req.body.username,req.user.username]);
+    if (usuarioCambiado) {
+        await db.query('UPDATE edificiosusuarios SET ? WHERE idUsuario = ?', [{ idUsuario: req.body.username },usuarioAnterior]);
+
+        await db.query('UPDATE historiallogueo SET ? WHERE username = ?',[{username: req.body.username},usuarioAnterior]);
+
+        await db.query('UPDATE mensajes SET ? WHERE remitente = ?',[{remitente: req.body.username},usuarioAnterior]);
+
+        await db.query('UPDATE mensajes SET ? WHERE destinatario = ?',[{destinatario: req.body.username},usuarioAnterior]);
+
+        await db.query('UPDATE registroactividad SET ? WHERE idUsuario = ?',[{idUsuario: req.body.username},usuarioAnterior]);
+    }
+
+    await db.query('UPDATE edificiosusuarios SET ? WHERE idUsuario = ? AND idAdmin = ?', [nuevoEdificioUsuario, req.body.username, req.user.username]);
 
     req.flash('mensaje', 'Usuario modificado exitosamente');
     return res.redirect('/usuarios');
